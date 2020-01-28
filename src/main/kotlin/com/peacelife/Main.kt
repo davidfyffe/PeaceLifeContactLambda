2d1da0af88c24df6170f98528e4f8a8dc8cdf710
davidfyffe@yahoo.co.uk <Dromore@1>
2020-01-28 23:24:36 +0000

    adding main for testing locally and adding custom filter to stop cross site access

--- src/main/kotlin/com/peacelife/Main.kt
+++ src/main/kotlin/com/peacelife/Main.kt
@@ -6,16 +6,24 @@ import com.amazonaws.services.sns.model.PublishRequest
 import org.http4k.core.*
 import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Status.Companion.PRECONDITION_FAILED
 import org.http4k.filter.CorsPolicy
 import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson.auto
+import org.http4k.lens.Header
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
 import org.http4k.serverless.AppLoader
 import org.joda.time.DateTime
 
 data class MessageBody(val name: String, val email: String, val message: String)
+
+fun main() {
+    internalRoute().asServer(SunHttp(1234))
+}
 
 object PeaceLifeLambda : AppLoader {
     override fun invoke(env: Map<String, String>): HttpHandler {
@@ -23,13 +31,25 @@ object PeaceLifeLambda : AppLoader {
     }
 }
 
+const val ORIGIN = "https://davidfyffe.github.io"
+
 val corsPolicy : CorsPolicy =
-        CorsPolicy(origins = listOf("https://davidfyffe.github.io"),
+        CorsPolicy(origins = listOf(ORIGIN),
                 headers = listOf("content-type", "Origin"),
                 methods = listOf(Method.POST, Method.OPTIONS))
 
+// Default CORS behaviour will set access-control-allow-origin header to string 'null' if the Origin is not expected.
+// However, it will by default run the request, just deny the sender the response. Fuck that.
+val myCorsFilter = Filter { next: HttpHandler ->
+    { request: Request ->
+        val allowedOrigins = Header.required("access-control-allow-origin")
+        val origin = allowedOrigins(request)
+        if(origin == "null") Response(PRECONDITION_FAILED).body("Incorrect CORS origin supplied") else next(request)
+    }
+}
+
 fun internalRoute(sns: AmazonSNS = AmazonSNSClientBuilder.standard().build()): RoutingHttpHandler {
-    return ServerFilters.Cors(corsPolicy).then(
+    return ServerFilters.Cors(corsPolicy).then(myCorsFilter).then(
             routes(
                     "/contact" bind Method.POST to invokeSNS(sns)
             )
